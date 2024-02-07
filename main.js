f2abb65fcf78fc1c5972a720e8ea973ee67361bb
v.melekhin <v.melekhin@timeweb.ru>
2024-02-07 17:26:31 +0300

    Revert "test hook 222"
    This reverts commit 2af99c5978d3aac354acd85c5c6ed7f8ddaaf124.

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const main = express();
+require('dotenv').config();
+const port = 8000;
+
+main.get('/', (req, res) => {
+    res.send(`Hello, World! ${process.env.TITLE}`);
+});
+
+
+
+
+main.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
